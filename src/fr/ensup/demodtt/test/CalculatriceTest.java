e4b20362c6160cd2496f86bae9e4793470b29dce
aminedarabid <amine.darabid@gmail.com>
2019-03-06 16:59:39 +0100

    V1 : scenario failed

---
+++ src/fr/ensup/demodtt/test/CalculatriceTest.java
@@ -0,0 +1,11 @@
+package fr.ensup.demodtt.test;
+
+import org.junit.Test;
+
+public class CalculatriceTest {
+
+	@Test
+	public void scenarioAdditionEntier() {
+		org.junit.Assert.fail("echec");
+	}
+}
